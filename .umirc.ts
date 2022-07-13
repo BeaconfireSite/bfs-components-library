12ae9657a7f800a5a097df7cf2d0d502174c1343
Zhiyue Li <zhiyue.li.joy@gmail.com>
2022-07-13 17:38:49 -0400

    update

--- .umirc.ts
+++ .umirc.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'dumi';
 
-const repo = 'dumi-test';
+const repo = 'bfs-components-library';
 
 export default defineConfig({
   title: repo,
@@ -18,7 +18,7 @@ export default defineConfig({
     null,
     {
       title: 'GitHub',
-      path: 'https://github.com/Zhiyue-Li/dumi-test',
+      path: 'https://github.com/BeaconfireSite/bfs-components-library',
     },
   ],
   // more config: https://d.umijs.org/config
